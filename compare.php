a532ed22246f020ca94e74f82cedda23f90683a9
Anup Agarwal <108anup@gmail.com>
2015-12-12 13:09:50 +0530

    Update portal.php

--- compare.php
+++ compare.php
@@ -3,38 +3,38 @@
 <div class="jumbotron text-center">
   <h1><a id ="heading" href="index.php">Course Comparator</a></h1><br><br>
 
-  <div class="row">
+    <div class="row">
 
-		<div class="col-sm-2 col-sm-offset-2">
-            <label class="control-label top-buffer">Select Course</label>
+    	<div class="col-sm-2 col-sm-offset-2">
+              <label class="control-label top-buffer">Select Course</label>
+          </div>
+
+        <div class="col-sm-4">
+
+        	<select id="Course_Name" class="form-control" name="Course_Name">
+    	    	<?php
+
+    		        $sql="SELECT DISTINCT Course_Name FROM Courses WHERE branchID='{$branchID}';";
+
+    		   		if ($result = $conn->query($sql)) {
+    		   			while($row = $result->fetch_assoc())
+    			        {
+    			            echo "<option value={$row['Course_Name']}>{$row['Course_Name']}</option>";
+    			        }
+    		   		} else {
+    		   			echo '<option value="">No Courses</option>';
+    		   		}
+
+    	    	?>
+        	</select>
+
         </div>
 
-	    <div class="col-sm-4">
+      	<div class="col-sm-2">
+      		<button class="btn btn-danger col-sm-4" id="enterbtn">Enter</button>
+    	</div>
 
-	    	<select id="Course_Name" class="form-control" name="Course_Name">
-		    	<?php
-
-			        $sql="SELECT DISTINCT Course_Name FROM Courses WHERE branchID='{$branchID}';";
-
-			   		if ($result = $conn->query($sql)) {
-			   			while($row = $result->fetch_assoc())
-				        {
-				            echo "<option value={$row['Course_Name']}>{$row['Course_Name']}</option>";
-				        }
-			   		} else {
-			   			echo '<option value="">No Courses</option>';
-			   		}
-
-		    	?>
-	    	</select>
-
-	    </div>
-
-    	<div class="col-sm-2">
-    		<button class="btn btn-danger col-sm-4" id="enterbtn">Enter</button>
-		</div>
-
-	</div>
+    </div>
 
 </div>
 
--- portal.php
+++ portal.php
@@ -1,10 +1,32 @@
 <?php include 'start.php';?>
+
+<div class="jumbotron text-center">
+  <h1><a id="heading" href="index.php">Course Comparator</a></h1><br><br>
+</div>
+
 <?php
+    $sql="SELECT institute FROM Users WHERE username='{$username}';";
+    if ($result = $conn->query($sql)){
+      while($row = $result->fetch_assoc()){
+            $user_institute = {$row['institute']};
+        }
+    }
+    $sql="SELECT DISTINCT Course_Name FROM Courses WHERE institite='{$user_institute}';";
 
-
-
-
+    if ($result = $conn->query($sql)) {
+      while($row = $result->fetch_assoc())
+        {
+            echo "{$row['Course_Name']} - {$Branch}<br>{$row['Course_Details']}";
+        }
+    } else {
+      echo 'No Courses';
+    }
 
 ?>
+
+<br><br><br>
+<br><br><br>
+<br><br><br>
+<br><br><br>
 
 <?php include 'end.php' ?>
